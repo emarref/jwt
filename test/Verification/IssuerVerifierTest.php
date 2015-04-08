c3fe34a378702d16946b0c2b36a18aca24c07053
Malcolm Fell <emarref@gmail.com>
2015-04-08 14:12:49 +1200

    Add test for issuer

---
+++ test/Verification/IssuerVerifierTest.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace Emarref\Jwt\Verification;
+
+use Emarref\Jwt\Claim;
+
+class IssuerVerifierTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $payload;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $token;
+
+    public function setUp()
+    {
+        $this->payload = $this->getMockBuilder('Emarref\Jwt\Token\Payload')->getMock();
+
+        $this->token = $this->getMockBuilder('Emarref\Jwt\Token')->getMock();
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Cannot verify invalid issuer value.
+     */
+    public function testInvalidIssuer()
+    {
+        new IssuerVerifier(new \stdClass());
+    }
+
+    public function testNoIssuer()
+    {
+        $this->token->expects($this->once())
+            ->method('getPayload')
+            ->will($this->returnValue($this->payload));
+
+        $this->payload->expects($this->once())
+            ->method('findClaimByName')
+            ->with(Claim\Issuer::NAME)
+            ->will($this->returnValue(null));
+
+        $verifier = new IssuerVerifier();
+        $verifier->verify($this->token);
+    }
+
+    /**
+     * @expectedException Emarref\Jwt\Exception\VerificationException
+     * @expectedExceptionMessage Issuer is invalid.
+     */
+    public function testIssuerInPayloadOnly()
+    {
+        $issuerClaim = $this->getMockBuilder('Emarref\Jwt\Claim\Issuer')
+            ->getMock();
+
+        $issuerClaim->expects($this->once())
+            ->method('getValue')
+            ->will($this->returnValue('an_issuer'));
+
+        $this->token->expects($this->once())
+            ->method('getPayload')
+            ->will($this->returnValue($this->payload));
+
+        $this->payload->expects($this->once())
+            ->method('findClaimByName')
+            ->with(Claim\Issuer::NAME)
+            ->will($this->returnValue($issuerClaim));
+
+        $verifier = new IssuerVerifier();
+        $verifier->verify($this->token);
+    }
+
+    /**
+     * @expectedException Emarref\Jwt\Exception\VerificationException
+     * @expectedExceptionMessage Issuer is invalid.
+     */
+    public function testIssuerInContextOnly()
+    {
+        $this->token->expects($this->once())
+            ->method('getPayload')
+            ->will($this->returnValue($this->payload));
+
+        $this->payload->expects($this->once())
+            ->method('findClaimByName')
+            ->with(Claim\Issuer::NAME)
+            ->will($this->returnValue(null));
+
+        $verifier = new IssuerVerifier('an_issuer');
+        $verifier->verify($this->token);
+    }
+
+    /**
+     * @expectedException Emarref\Jwt\Exception\VerificationException
+     * @expectedExceptionMessage Issuer is invalid.
+     */
+    public function testIssuerMismatch()
+    {
+        $issuerClaim = $this->getMockBuilder('Emarref\Jwt\Claim\Issuer')
+            ->getMock();
+
+        $issuerClaim->expects($this->once())
+            ->method('getValue')
+            ->will($this->returnValue('an_issuer'));
+
+        $this->token->expects($this->once())
+            ->method('getPayload')
+            ->will($this->returnValue($this->payload));
+
+        $this->payload->expects($this->once())
+            ->method('findClaimByName')
+            ->with(Claim\Issuer::NAME)
+            ->will($this->returnValue($issuerClaim));
+
+        $verifier = new IssuerVerifier('some_other_issuer');
+        $verifier->verify($this->token);
+    }
+
+    public function testSuccess()
+    {
+        $issuerClaim = $this->getMockBuilder('Emarref\Jwt\Claim\Issuer')
+            ->getMock();
+
+        $issuerClaim->expects($this->once())
+            ->method('getValue')
+            ->will($this->returnValue('an_issuer'));
+
+        $this->token->expects($this->once())
+            ->method('getPayload')
+            ->will($this->returnValue($this->payload));
+
+        $this->payload->expects($this->once())
+            ->method('findClaimByName')
+            ->with(Claim\Issuer::NAME)
+            ->will($this->returnValue($issuerClaim));
+
+        $verifier = new IssuerVerifier('an_issuer');
+        $verifier->verify($this->token);
+    }
+}
