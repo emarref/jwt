deedf25af18be4dafa697911e93d66bafa646cc3
Malcolm Fell <emarref@gmail.com>
2015-04-08 14:16:57 +1200

    Add test for subject verifier

---
+++ test/Verification/SubjectVerifierTest.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace Emarref\Jwt\Verification;
+
+use Emarref\Jwt\Claim;
+
+class SubjectVerifierTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $payload;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $token;
+
+    public function setUp()
+    {
+        $this->payload = $this->getMockBuilder('Emarref\Jwt\Token\Payload')->getMock();
+
+        $this->token = $this->getMockBuilder('Emarref\Jwt\Token')->getMock();
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Cannot verify invalid subject value.
+     */
+    public function testInvalidSubject()
+    {
+        new SubjectVerifier(new \stdClass());
+    }
+
+    public function testNoSubject()
+    {
+        $this->token->expects($this->once())
+            ->method('getPayload')
+            ->will($this->returnValue($this->payload));
+
+        $this->payload->expects($this->once())
+            ->method('findClaimByName')
+            ->with(Claim\Subject::NAME)
+            ->will($this->returnValue(null));
+
+        $verifier = new SubjectVerifier();
+        $verifier->verify($this->token);
+    }
+
+    /**
+     * @expectedException Emarref\Jwt\Exception\VerificationException
+     * @expectedExceptionMessage Subject is invalid.
+     */
+    public function testSubjectInPayloadOnly()
+    {
+        $subjectClaim = $this->getMockBuilder('Emarref\Jwt\Claim\Subject')
+            ->getMock();
+
+        $subjectClaim->expects($this->once())
+            ->method('getValue')
+            ->will($this->returnValue('a_subject'));
+
+        $this->token->expects($this->once())
+            ->method('getPayload')
+            ->will($this->returnValue($this->payload));
+
+        $this->payload->expects($this->once())
+            ->method('findClaimByName')
+            ->with(Claim\Subject::NAME)
+            ->will($this->returnValue($subjectClaim));
+
+        $verifier = new SubjectVerifier();
+        $verifier->verify($this->token);
+    }
+
+    /**
+     * @expectedException Emarref\Jwt\Exception\VerificationException
+     * @expectedExceptionMessage Subject is invalid.
+     */
+    public function testSubjectInContextOnly()
+    {
+        $this->token->expects($this->once())
+            ->method('getPayload')
+            ->will($this->returnValue($this->payload));
+
+        $this->payload->expects($this->once())
+            ->method('findClaimByName')
+            ->with(Claim\Subject::NAME)
+            ->will($this->returnValue(null));
+
+        $verifier = new SubjectVerifier('a_subject');
+        $verifier->verify($this->token);
+    }
+
+    /**
+     * @expectedException Emarref\Jwt\Exception\VerificationException
+     * @expectedExceptionMessage Subject is invalid.
+     */
+    public function testSubjectMismatch()
+    {
+        $subjectClaim = $this->getMockBuilder('Emarref\Jwt\Claim\Subject')
+            ->getMock();
+
+        $subjectClaim->expects($this->once())
+            ->method('getValue')
+            ->will($this->returnValue('a_subject'));
+
+        $this->token->expects($this->once())
+            ->method('getPayload')
+            ->will($this->returnValue($this->payload));
+
+        $this->payload->expects($this->once())
+            ->method('findClaimByName')
+            ->with(Claim\Subject::NAME)
+            ->will($this->returnValue($subjectClaim));
+
+        $verifier = new SubjectVerifier('some_other_subject');
+        $verifier->verify($this->token);
+    }
+
+    public function testSuccess()
+    {
+        $subjectClaim = $this->getMockBuilder('Emarref\Jwt\Claim\Subject')
+            ->getMock();
+
+        $subjectClaim->expects($this->once())
+            ->method('getValue')
+            ->will($this->returnValue('a_subject'));
+
+        $this->token->expects($this->once())
+            ->method('getPayload')
+            ->will($this->returnValue($this->payload));
+
+        $this->payload->expects($this->once())
+            ->method('findClaimByName')
+            ->with(Claim\Subject::NAME)
+            ->will($this->returnValue($subjectClaim));
+
+        $verifier = new SubjectVerifier('a_subject');
+        $verifier->verify($this->token);
+    }
+}
