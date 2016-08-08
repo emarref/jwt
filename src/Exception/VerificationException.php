79522d876f38ffee54b183fdc85ea6af81a41eee
Malcolm Fell <emarref@gmail.com>
2016-08-09 09:34:28 +1200

    Add exception codes

--- src/Exception/VerificationException.php
+++ src/Exception/VerificationException.php
@@ -4,5 +4,10 @@ namespace Emarref\Jwt\Exception;
 
 class VerificationException extends \Exception
 {
-
+    const CODE_INVALID_SIGNATURE = 1;
+    const CODE_INVALID_AUDIENCE = 2;
+    const CODE_EXPIRED = 3;
+    const CODE_INVALID_ISSUER = 4;
+    const CODE_INVALID_SUBJECT = 5;
+    const CODE_INVALID_NOT_BEFORE = 6;
 }
