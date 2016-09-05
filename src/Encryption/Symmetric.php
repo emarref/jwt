e108b615c259a1f283b6f0b0432e832eec9c6cca
Malcolm Fell <emarref@gmail.com>
2016-09-06 08:21:52 +1200

    Use timing-safe hash comparision

--- src/Encryption/Symmetric.php
+++ src/Encryption/Symmetric.php
@@ -56,6 +56,44 @@ class Symmetric extends AbstractEncryption implements EncryptionInterface
      */
     public function verify($value, $signature)
     {
-        return $this->algorithm->compute($value) === $signature;
+        $computedValue = $this->algorithm->compute($value);
+
+        return $this->timingSafeEquals($signature, $computedValue);
+    }
+
+    /**
+     * A timing safe equals comparison.
+     *
+     * @see http://blog.ircmaxell.com/2014/11/its-all-about-time.html
+     *
+     * @param string $safe The internal (safe) value to be checked
+     * @param string $user The user submitted (unsafe) value
+     *
+     * @return boolean True if the two strings are identical.
+     */
+    public function timingSafeEquals($safe, $user)
+    {
+        if (function_exists('hash_equals')) {
+            return hash_equals($user, $safe);
+        }
+
+        $safeLen = strlen($safe);
+        $userLen = strlen($user);
+
+        /*
+         * In general, it's not possible to prevent length leaks. So it's OK to leak the length.
+         * @see http://security.stackexchange.com/questions/49849/timing-safe-string-comparison-avoiding-length-leak
+         */
+        if ($userLen != $safeLen) {
+            return false;
+        }
+
+        $result = 0;
+
+        for ($i = 0; $i < $userLen; $i++) {
+            $result |= (ord($safe[$i]) ^ ord($user[$i]));
+        }
+
+        return $result === 0;
     }
 }
